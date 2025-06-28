775f627f36318937675e041a968064fd0c05a43b
Sumit tripathi <sumittripathi8795@gmail.in>
2025-06-29 01:06:45 +0530

    New Pattern

--- pttern.cpp
+++ pttern.cpp
@@ -22,12 +22,12 @@ int main(){
 }
 
 
-*/
 
-// 10 10 10 10 10 
-// 10 10 10 10 10
-// 10 10 10 10 10
-// 10 10 10 10 10
+
+10 10 10 10 10
+10 10 10 10 10
+10 10 10 10 10
+10 10 10 10 10
 
 #include<iostream>
 using namespace std;
@@ -39,3 +39,22 @@ int main(){
         cout << endl;
     }
 }
+
+*/
+
+// 1 1 1 1 1 
+// 2 2 2 2 2 
+// 3 3 3 3 3 
+// 4 4 4 4 4 
+// 5 5 5 5 5
+
+#include <iostream>
+    using namespace std;
+    int main(){
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 5; j++){
+                cout << i << " ";
+            }
+            cout<< endl;
+        }
+    }
