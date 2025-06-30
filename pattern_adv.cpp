49211e9a773b414fc4458ce1584d5781d22fd3ad
Sumit tripathi <sumittripathi8795@gmail.in>
2025-07-01 00:36:16 +0530

    Add new number pattern output and supporting code in pattern_adv.cpp

--- pattern_adv.cpp
+++ pattern_adv.cpp
@@ -42,3 +42,23 @@ int main(){
 }
 */
 
+/*
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 5; i++){
+        for (int j = 1; j <= i; j++){
+            cout << i<<" ";
+        }
+        cout << endl;
+        cout<<"Thanks for supports";
+    }
+}
+*/
