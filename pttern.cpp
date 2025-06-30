cf6dcead5e82070fb889b0b15742d015c8d8cfb8
Sumit tripathi <sumittripathi8795@gmail.in>
2025-06-30 23:36:08 +0530

    Refactor pattern output functions in pttern.cpp for improved readability

--- pttern.cpp
+++ pttern.cpp
@@ -198,7 +198,7 @@ int main(){
 11 12 13 14 15
 16 17 18 19 20
 21 22 23 24 25
-*/
+
 
 #include<iostream>
 using namespace std;
@@ -212,3 +212,5 @@ int main(){
         cout << endl;
     }
 }
+
+*/
