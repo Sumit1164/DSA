b530496fca2b4156cdfe9b05089cbad3bf0b9b9a
Sumit tripathi <sumittripathi8795@gmail.in>
2025-06-30 23:59:13 +0530

    Refactor pattern_adv.cpp to improve structure and add missing output for number patterns

--- pattern_adv.cpp
+++ pattern_adv.cpp
@@ -5,7 +5,7 @@
 * * * * 
 * * * * *
 
-*/
+
 
 #include<iostream>
 using namespace std;
@@ -17,5 +17,26 @@ int main(){
         }
         cout << endl;
 
+    }
+}
+
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5 
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int row, col;
+    for (row = 1; row <= 5; row++){
+        for (col = 1; col <= row; col++){
+            cout << col << " ";
+        }
+        cout << endl;
     }
 }
