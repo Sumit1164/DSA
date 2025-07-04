4f6bc17747a54de2d7fee8e65fe01d318af53c17
Sumit tripathi <sumittripathi8795@gmail.in>
2025-07-04 16:48:42 +0530

    implement a new pattern (pyramid) output and improve code structure

--- pattern_adv2.cpp
+++ pattern_adv2.cpp
@@ -127,3 +127,20 @@ int main(){
 //     * * * * *
 //   * * * * * * * 
 // * * * * * * * * * 
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j, n;
+    for (i = 1; i <= 5; i++){
+        for (j = 1; j <= 5 - i; j++){
+            cout << "  ";
+        }
+        for (j = 1; j<=(i*2-1); j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
