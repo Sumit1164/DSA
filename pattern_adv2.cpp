e6f01d3389ffec5e99c3ae1f2ceb1825baef1c5c
Sumit tripathi <sumittripathi8795@gmail.in>
2025-07-04 17:06:19 +0530

    Add new pattern output and refactor code structure in pattern_adv2.cpp

--- pattern_adv2.cpp
+++ pattern_adv2.cpp
@@ -119,7 +119,7 @@ int main(){
     }
 }
 
-*/
+
 
 
 //         *
@@ -140,6 +140,35 @@ int main(){
         }
         for (j = 1; j<=(i*2-1); j++){
             cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+*/
+
+
+//         1
+//       1 2 1
+//     1 2 3 2 1
+//   1 2 3 4 3 2 1
+// 1 2 3 4 5 4 3 2 1
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j;
+    for (i = 1; i <= 5; i++){
+        for (j = 1; j <= 5 - i; j++){
+            cout << "  ";
+        }
+        for (j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        for (j = i - 1; j >= 1; j--){
+            cout << j << " ";
         }
         cout << endl;
     }
