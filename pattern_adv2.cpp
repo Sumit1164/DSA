e160d6e2598f41aa4a3df9fb3a66149686aefe42
Sumit tripathi <sumittripathi8795@gmail.in>
2025-07-04 14:37:44 +0530

    Refactor pattern_adv2.cpp to clean up commented code and improve structure

--- pattern_adv2.cpp
+++ pattern_adv2.cpp
@@ -2,8 +2,7 @@
 //          *  *
 //       *  *  *
 //    *  *  *  *
-// *  *  *  *  * 
-
+// *  *  *  *  *
 
 /*
 
@@ -20,7 +19,7 @@ int main(){
         cout << endl;
     }
 }
-*/
+
 
 
 //         1
@@ -39,6 +38,29 @@ int main(){
         }
         for (j = 1; j <= i; j++){
             cout << i<<" ";
+        }
+        cout << endl;
+    }
+}
+
+*/
+
+//         1
+//       1 2
+//     1 2 3
+//   1 2 3 4
+// 1 2 3 4 5
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j;
+    for (i = 1; i <= 5; i++){
+        for (j = 1; j <= 5 - i; j++){
+            cout << "  ";
+        }
+        for (j = 1; j <= i; j++){
+            cout << j<<" ";
         }
         cout << endl;
     }
