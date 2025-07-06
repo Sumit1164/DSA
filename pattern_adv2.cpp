d9a67cdca1854eb12d80a1946d611174b5985553
Sumit tripathi <sumittripathi8795@gmail.in>
2025-07-07 01:27:45 +0530

    Add inverted pyramid pattern implementation in pattern_adv2.cpp

--- pattern_adv2.cpp
+++ pattern_adv2.cpp
@@ -183,3 +183,20 @@ int main(){
 //       * * *
 //         *
 
+
+// 1st way to print inverted pyramid pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j;
+    for (i = 5; i >=1; i--){
+        for (j = 1; j <= 9- (2 * i - 1); j++){
+            cout << " ";
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
