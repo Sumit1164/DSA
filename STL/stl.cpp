9922c819cffbe598f9151ae1ba72ef2ba2b1a508
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-11 22:55:39 +0530

    Front, Back, at, empty use these methods

--- STL/stl.cpp
+++ STL/stl.cpp
@@ -11,19 +11,26 @@ int main(){
     s.push_back(40);
     s.push_back(50);
     // Remove elements from vector (last element)
-    s.pop_back();
+    // s.pop_back();
     //  Remove element from specific index
-    s.erase(s.begin() + 2);
+    // s.erase(s.begin() + 2);
     // Insert element at specific index
-    s.insert(s.begin() + 2, 100);
+    // s.insert(s.begin() + 2, 100);
     // Size of vector (number of elements)
-    cout<<"Size of vector: "<<s.size()<<endl;
+    // cout<<"Size of vector: "<<s.size()<<endl;
     // Clear the vector
     // s.clear();
     // Capacity of vector (total allocated space)
-    cout<<"Capacity of vector: "<<s.capacity()<<endl;
+    // cout<<"Capacity of vector: "<<s.capacity()<<endl;
     for(int i=0; i<s.size(); i++){
         cout<<s[i]<<" ";
     }
+    cout << endl;
+
+    // Front, Back, at, empty
+    cout <<"Front element " <<s.front() << endl; // first element
+    cout <<"Back element " <<s.back() << endl; // last element
+    cout <<"Empty status " <<s.empty() << endl;  // 0 means false, 1 means true
+    cout <<"Element at index " <<s.at(2) << endl; // element at index 2
     return 0;
 }
