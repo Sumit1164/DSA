d4b949e756e26a116c08859c17b2e382844c12b4
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-11 22:36:16 +0530

    vector and it's methods

--- STL/stl.cpp
+++ STL/stl.cpp
@@ -2,17 +2,26 @@
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> s = {5};
+    // initialization of vector
+    vector<int> s;
+    // Insert elements in vector
+    s.push_back(10);
     s.push_back(20);
     s.push_back(30);
     s.push_back(40);
     s.push_back(50);
-    s.push_back(60);
-    s.push_back(70);
-    s.push_back(80);
+    // Remove elements from vector (last element)
     s.pop_back();
+    //  Remove element from specific index
     s.erase(s.begin() + 2);
+    // Insert element at specific index
+    s.insert(s.begin() + 2, 100);
+    // Size of vector (number of elements)
+    cout<<"Size of vector: "<<s.size()<<endl;
+    // Clear the vector
     // s.clear();
+    // Capacity of vector (total allocated space)
+    cout<<"Capacity of vector: "<<s.capacity()<<endl;
     for(int i=0; i<s.size(); i++){
         cout<<s[i]<<" ";
     }
