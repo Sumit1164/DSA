ca1e5f8577183b346fbddb1c6f0ea091a78bc386
Sumit tripathi <sumittripathi8795@gmail.com>
2026-01-28 00:59:43 +0530

    DIVIDE ARRAY IN 2 SUBARRAY WITH EQUAL SUM

--- Subarray.cpp
+++ Subarray.cpp
@@ -1,6 +1,9 @@
 // DIVIDE ARRAY IN 2 SUBARRAY WITH EQUAL SUM
 #include <iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+/*
 int main()
 {
     int arr[] = {3, 4, -2, 5, 8, 20, -10, 8};
@@ -18,4 +21,36 @@ int main()
         cout << sum1 << " " << sum2;
     }
     return 0;
+}
+*/
+
+bool divide(vector<int> arr){
+    int max = 500, prefix = 0, total_sum = 0, n = arr.size();
+    for (int i = 0; i < n; i++){
+        total_sum += arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        prefix += arr[i];
+        int ans = total_sum - prefix;
+        if (ans == prefix)
+        {
+            return 1;
+        }
+        
+    }
+    return 0;
+}
+
+int main(){
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    vector<int> v(n);
+    cout << "Enter the element in the array: ";
+    for (int i = 0; i <n; i++)
+    {
+        cin >> v[i];
+    }
+    cout << divide(v);
 }
