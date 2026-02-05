201a45983ad2a47cf3aca9dbfe5802e3d1edbd64
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-05 17:28:00 +0530

    pattern next level

--- ptrn1.cpp
+++ ptrn1.cpp
@@ -50,7 +50,7 @@ int main(){
         cout << endl;
     }
 
-    */
+
 
     int row, col;
     for (row = 1; row <= 5; row = row+1)
@@ -60,7 +60,14 @@ int main(){
         }
         cout << endl;
     }
-    
+    */
 
-    return 0;
+    for (int i = 1; i <= 5; i++){
+        for (int j = 1; j <= 5 - (i - 1); j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+        return 0;
 }
