acf2f4e82c027bb8a50aba9d634916888a0e3f08
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-05 15:22:43 +0530

    pattern next level

--- ptrn1.cpp
+++ ptrn1.cpp
@@ -42,9 +42,10 @@ int main(){
     */
 
     for (int row = 1; row <= 5; row++){
-        char name = 'a' + (row - 1);
+        
         for (int col = 1; col <= row; col++)
         {
+            char name = 'a' + (row - 1);
             cout << name << " ";
         }
         cout << endl;
