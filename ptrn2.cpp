6c32bd9aa8c70b98f3714887a198754682be09a0
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-07 02:09:01 +0530

    advance pattern printing

--- ptrn2.cpp
+++ ptrn2.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i, j;
-    for (int i = 0; i <= 5; i++)
+    int i, j, n;
+    cout << "Enter the length: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
     {
-        
+        for (int j = 1; j<= n-i; j++)
+            cout <<"  ";
+        for (int j = 1; j<=i; j++)
+        {
+                cout <<"* ";
+        }
+        cout << endl;
+
     }
-    
+
+
     return 0;
 }
