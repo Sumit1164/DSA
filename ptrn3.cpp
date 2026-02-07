998566a3c58b7f76d9ed1aafad1a672a0ad5f486
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-07 17:52:31 +0530

    pattern printing advance ++ such as numbe of pyramid

--- ptrn3.cpp
+++ ptrn3.cpp
@@ -1,12 +1,34 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for (int row = 1; row <= 5; row++){
-        for (int col = 1; col <= 5 - row; col = col + 1){
+    /*
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    for (int row = 1; row <= n; row++){
+        for (int col = 1; col <= n - row; col = col + 1){
             cout << "  ";
         }
         for (int col = 1; col<= 2 * row - 1; col++){
             cout << "* ";
+        }
+        cout << endl;
+    }
+    */
+
+    int n, row, col;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    for (row = 1; row <= n; row++){
+        for (col = 1; col <= n - row; col++){
+            cout << "  ";
+        }
+        for (col = 1; col <= row; col++){
+            cout << col << " ";
+        }
+        for (col = row - 1; col >= 1; col--){
+            cout << col << " ";
         }
         cout << endl;
     }
