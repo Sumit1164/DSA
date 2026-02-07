7a0fb084f99b37bac3fc6754d8132a6529a59115
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-07 17:21:13 +0530

    pattern printing advance ++ such as pyramid

---
+++ ptrn3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int row = 1; row <= 5; row++){
+        for (int col = 1; col <= 5 - row; col = col + 1){
+            cout << "  ";
+        }
+        for (int col = 1; col<= 2 * row - 1; col++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
