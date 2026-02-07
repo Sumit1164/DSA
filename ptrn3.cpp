f111d6d8a703e286330f3e4853fc332102b276c2
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-07 23:24:46 +0530

    pattern printing advance ++ such as opposite pyramid

--- ptrn3.cpp
+++ ptrn3.cpp
@@ -50,5 +50,28 @@ int main(){
 
     */
 
-    return 0;
+    int row, col, n;
+    cout << "Enter the number: ";
+    cin >> n;
+    for (row = n; row >= 1; row--){
+        for (col = 1; col <= row; col++)
+            cout << "* ";
+        for (col = 1; col <= 2 * n - 2 * row; col++)
+            cout << "  ";
+        for (col = 1; col <= row; col++)
+            cout << "* ";
+        cout << endl;
+    }
+
+    for (row = 1; row <= n; row++){
+        for (col = 1; col <= row; col++)
+            cout << "* ";
+        for (col = 1; col <= 2 * n - 2 * row; col++)
+            cout << "  ";
+        for (col = 1; col <= row; col++)
+            cout << "* ";
+        cout << endl;
+    }
+
+        return 0;
 }
