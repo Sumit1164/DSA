e6fb493aa6f524f38fd125816db042e051426415
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-14 00:42:41 +0530

    BINARY TO DECIMAL

--- BinaryToDecimal.cpp
+++ BinaryToDecimal.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 using namespace std;
+
+/*
 int main(){
     int i, ans = 0, num, rem, mul = 1;
     cout << "Enter the number: ";
@@ -18,3 +20,36 @@ int main(){
     }
     cout << ans << endl;
 }
+
+*/
+
+
+// !  ------------> BINARY TO DECIMAL 
+
+int main(){
+    int num;
+    int ans = 0, rem, mul = 1;
+    cout << "Enter the number: ";
+    cin >> num;
+    while (num>0)
+    {
+        // reminder
+        rem = num % 10;
+        // number divide
+        num /= 10;
+        // ans
+        ans += mul * rem;
+        // mul
+        mul *= 2;
+    }
+    cout << ans ;
+}
+
+
+
+
+
+
+
+
+// ! ----------> Decimal to octagan 
