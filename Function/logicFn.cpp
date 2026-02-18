98871598fed97348a7060f7558670dda35a75f3d
Sumit tripathi <sumittripathi8795@gmail.com>
2026-02-18 23:46:45 +0530

    Implement prime number check and factorial calculation for user inputs

---
+++ Function/logicFn.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+bool Prime(int a)
+{
+    if (a < 2)
+        return false;
+
+    for (int i = 2; i * i <= a; i++)
+    {
+        if (a % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+long long Factorial(int a)
+{
+    long long fact = 1;
+
+    for (int i = 1; i <= a; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+
+int main()
+{
+
+    int a, b;
+    cout << "Enter the value of a and b: ";
+    cin >> a >> b;
+
+    // Prime check
+    if (Prime(a))
+        cout << a << " is Prime" << endl;
+    else
+        cout << a << " is Not Prime" << endl;
+
+    if (Prime(b))
+        cout << b << " is Prime" << endl;
+    else
+        cout << b << " is Not Prime" << endl;
+
+    // Factorial
+    cout << "Factorial of " << a << " is " << Factorial(a) << endl;
+    cout << "Factorial of " << b << " is " << Factorial(b) << endl;
+
+    return 0;
+}
