d31c7d1e2b0fcfdd87927c9617b1a18cb3257e77
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-23 21:35:26 +0530

    selection sort using C++

---
+++ Sorting/selectionSORT.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[7] = {5, 7, 3, 2, 9, 1, 4};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i <= n -1; i++)
+    {
+        int index = i;
+
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j] < arr[index])
+            {
+                index = j;
+            }
+            
+        }
+
+        swap(arr[i], arr[index]);
+    }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+}
+
+/*
+        int n = nums.size();
+        for(int i = 0; i < n-1; i++){
+            int index = i;
+            for(int j = i + 1; j < n; j++){
+                if(nums[j]< nums[index])
+                    index = j;
+            }
+            swap(nums[i], nums[index]);
+        }
+        return nums;
+
+*/
