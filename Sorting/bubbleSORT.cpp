c437fcd99f7b63523b737f5445582957bf61c06e
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-24 11:28:18 +0530

    bubble sort

---
+++ Sorting/bubbleSORT.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {7, 4, 8, 5, 3};
+    int i, j;
+    for (j = 0; j<5; j++)
+    {
+        bool swapped = 0;
+        for (i=0; i<5-1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+                swap(arr[i], arr[i + 1]);
+            swapped = 1;
+        }
+        if (swapped == 0)
+            break;
+    }
+    for ( i = 0; i < 5; i++)
+        cout << arr[i] << " ";  
+}
