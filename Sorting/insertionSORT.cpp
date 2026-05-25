7735d21527f2473da4501de61e3f582a1772d324
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-26 01:05:56 +0530

    insertion sort using Descending Order

--- Sorting/insertionSORT.cpp
+++ Sorting/insertionSORT.cpp
@@ -1,12 +1,39 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int arr[5] = {7, 4, 6, 5, 0};
+//     for (int i = 1; i < 5; i++)
+//     {
+//         for (int j = i; j > 0; j--){
+//             if (arr[j] < arr[j-1])
+//                 swap(arr[j], arr[j - 1]);
+//             else
+//                 break;
+//         }
+//     }
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+    
+// }
+
+
+
+// Descending Order
+
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int arr[5] = {7, 4, 6, 5, 0};
     for (int i = 1; i < 5; i++)
     {
-        for (int j = i; j > 0; j--){
-            if (arr[j] < arr[j-1])
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] > arr[j - 1])
                 swap(arr[j], arr[j - 1]);
             else
                 break;
@@ -16,5 +43,4 @@ int main(){
     {
         cout << arr[i] << " ";
     }
-    
 }
