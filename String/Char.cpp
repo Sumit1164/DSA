07c98a303b37dd94a143b38f1b0942b0682dc8c7
Sumit tripathi <sumittripathi8795@gmail.com>
2026-06-12 00:15:56 +0530

    char vs string

---
+++ String/Char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // char arr[] = {'S', 'U', 'M', 'I', 'T'};
+    // for (int i = 0; i < 5; i++)
+    //     cout << arr[i] << " ";
+
+    char arr[10];
+    cin >> arr;
+    cout << "This is characters: " << arr;
+}
