005df7e7f9fa0106a966cfe2652b59d625736e2a
Sumit tripathi <sumittripathi8795@gmail.com>
2026-06-12 00:16:14 +0530

    string in c++

---
+++ String/String.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // string s = "Sumit Tripathi";
+    // cout << s;
+
+    // User input
+    string s;
+    cout << "Enter your name: ";
+    cin >> s;
+    cout << "You are: " << s;
+}
