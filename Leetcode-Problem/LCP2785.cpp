30a23c376fc404746fd114ab0504448a211595bb
Sumit tripathi <sumittripathi8795@gmail.com>
2026-06-18 21:41:04 +0530

    solve a leet code problem 2785

---
+++ Leetcode-Problem/LCP2785.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    string s = "leEtcOde";
+    vector<int> lower(26, 9), upper(26, 0);
+    string ans;
+
+    for (int i = 0; i < s.size(); i++){
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i]=='u'){
+            lower[s[i] - 'a']++;
+            s[i] = '#';
+        }
+        else if ( s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+            upper[s[i] - 'A']++;
+            s[i] = '#';
+        }
+    }
+
+    for (int i = 0; i < 26; i++){
+        char c = 'A' + i;
+        while (upper[i])
+        {
+            ans += c;
+            upper[i]--;
+        }  
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        char c = 'a' + i;
+        while (lower[i])
+        {
+            ans += c;
+            lower[i]--;
+        }
+        
+    }
+
+    int first = 0;
+    int second = 0;
+    while(second < ans.size()){
+        if(s[first]=='#'){
+            s[first] = ans[second];
+            second++;
+        }
+        first++;
+    }
+
+    cout << s;
+}
