15d0c2416c3f0e1a4165b369d48607476abc62fc
Sumit tripathi <sumittripathi8795@gmail.com>
2026-06-30 21:33:19 +0530

    some features

--- Pointer/pointer.cpp
+++ Pointer/pointer.cpp
@@ -12,7 +12,18 @@ int main()
     cout << "Size of pointer: " << sizeof(ptr)<<endl;
     string s = "S";
     string *ptr1 = &s;
-    cout << "The address of s is: " << ptr1;
+    cout << "The address of s is: " << ptr1 << endl;
 
-    // 
+    //
+
+    int x = 12;
+    // print the address of a
+    int *ptrX = &x;
+    cout << "Address of X: " << ptrX << endl;
+
+    cout << "The value of X print by using pointer: " << *ptrX << endl;
+    int y = 57;
+    ptrX = &y;
+    cout << "The address of Y: " << ptrX << endl;
+    cout << "The value of Y print by using pointer: " << *ptrX << endl;
 }
