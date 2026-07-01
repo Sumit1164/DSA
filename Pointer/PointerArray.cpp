286b661b111fdfdbc3848d049aa3e5a0c4a55813
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-01 23:47:22 +0530

    pointer relatioonships with array

---
+++ Pointer/PointerArray.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 4, 5, 7};
+    // Print the address of first element, There are three ways to print the array index 0 value
+    cout << arr << endl;
+    // cout << arr + 0 << endl;
+    // cout << &arr[0] << endl;
+    
+    // Using pointer to print the address
+    int *ptr = arr; //1way
+    int *ptr1 = &arr[0]; // 2 way
+    cout << ptr << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *(arr + i) << endl;
+    }
+
+    cout << endl;
+
+    // Print all elements addresses
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr + i << endl;
+    }
+    
+}
