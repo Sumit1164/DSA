ad0d382e13dd092795445911398bc2172789f192
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-01 23:53:42 +0530

    Print the value

--- Pointer/PointerArray.cpp
+++ Pointer/PointerArray.cpp
@@ -5,26 +5,32 @@ int main()
 {
     int arr[5] = {1, 2, 4, 5, 7};
     // Print the address of first element, There are three ways to print the array index 0 value
-    cout << arr << endl;
-    cout << arr + 0 << endl;
-    cout << &arr[0] << endl;
+    // cout << arr << endl;
+    // cout << arr + 0 << endl;
+    // cout << &arr[0] << endl;
     
     // Using pointer to print the address
-    int *ptr = arr; //1way
-    int *ptr1 = &arr[0]; // 2 way
-    cout << ptr << endl;
+    // int *ptr = arr; //1way
+    // int *ptr1 = &arr[0]; // 2 way
+    // cout << ptr << endl;
 
-    for (int i = 0; i < 5; i++)
-    {
-        cout << *(arr + i) << endl;
-    }
+    // Print the address of second element, There are three ways to print the array index 1 value
+    // cout << arr + 1 << endl;
 
+
+    // Print the value
+    cout << arr[0];
     cout << endl;
-
+    
     // Print all elements addresses
     for (int i = 0; i < 5; i++)
     {
         cout << arr + i << endl;
     }
     
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *(arr + i) << endl;
+    }
 }
