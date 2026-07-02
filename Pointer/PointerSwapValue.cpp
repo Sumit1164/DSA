3cba5e91b5635703d0aa737f0d86cbfaa3257675
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-02 23:44:22 +0530

    swap to values without 3rd variable

---
+++ Pointer/PointerSwapValue.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void swapped(int *n, int *m){
+    *n = *n + *m;
+    *m = *n - *m;
+    *n = *n - *m;
+
+    // a = a+b = 25
+    // b = a-b = 10
+    // a = a-b = 15
+}
+int main()
+{
+    int first = 10;
+    int second =15;
+
+    swapped(&first, &second);
+    cout << first << " " << second;
+}
