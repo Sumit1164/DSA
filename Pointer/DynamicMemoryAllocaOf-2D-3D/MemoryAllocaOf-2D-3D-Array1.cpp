ea361c3a3f1333f12716670f2fc909f1c30e46bc
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-06 18:30:49 +0530

    memory allocation of 3D array in Heap

---
+++ Pointer/DynamicMemoryAllocaOf-2D-3D/MemoryAllocaOf-2D-3D-Array1.cpp
@@ -0,0 +1,47 @@
+// 3D array
+#include<iostream>
+using namespace std;
+int main()
+{
+    int L, B, H;
+    cout << "Enter the L, B and H : ";
+    cin >> L >> B >> H;
+
+    cout << "---------" << endl;
+    int ***ptr = new int**[L];
+    // Create 2D array and store it's address in ptr
+    for (int i = 0; i < L; i++)
+    {
+        ptr[i] = new int*[B];
+
+        for (int j = 0; j < B; j++)
+        {
+            ptr[i][j] = new int[H];
+        }
+    }
+
+    for (int i = 0; i < L; i++)
+    {
+        for (int j = 0; j < B; j++)
+        {
+            for (int k = 0; k < H; k++)
+            {
+                cin >> ptr[i][j][k];
+            }
+        }
+    }
+
+    for (int i = 0; i < L; i++)
+    {
+        for (int j = 0; j < B; j++)
+        {
+            for (int k = 0; k < H; k++)
+            {
+                cout << ptr[i][j][k] << " ";
+            }
+
+            cout << endl;
+        }
+    }
+
+}
