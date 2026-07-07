eff29eaf2e65455d41590ee90ddb2b93207c89a1
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-08 00:24:27 +0530

    Print n to 1 using recursive approach

--- Recursion/RecursionPrintNto1.cpp
+++ Recursion/RecursionPrintNto1.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 using namespace std;
+
+void print (int n)
+{
+    if(n==1){
+        cout << n;
+        return;
+    }
+    cout << n << endl;
+    print(n - 1);
+}
+
 int main()
 {
     // Print n to 1 using itrative approach
     int n = 5;
-    for (int i = n; i >0; i--)
-    {
-        cout << i << endl;
-    }
+    // for (int i = n; i >0; i--)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // Print n to 1 using recursive approach
+    print(n);
 }
