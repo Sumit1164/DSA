224e1e09dc3e4ebfc31b7abe38d3c8791f827265
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-08 02:09:56 +0530

    solve a problem using recursion to print odd numbers N to 1.

---
+++ Recursion/Problems/2Problem.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void print(int s)
+{
+    if(s ==1)
+    {
+        cout << s << endl;
+        return;
+    }
+    cout << s << endl;
+    print(s - 2);
+}
+
+int main()
+{
+    int n = 9;
+    print(n);
+}
