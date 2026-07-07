b298b8b543872f59986cc7e1074f6cfebed37057
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-08 00:20:47 +0530

    Print n to 1 using itrative approach

---
+++ Recursion/RecursionPrintNto1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Print n to 1 using itrative approach
+    int n = 5;
+    for (int i = n; i >0; i--)
+    {
+        cout << i << endl;
+    }
+}
