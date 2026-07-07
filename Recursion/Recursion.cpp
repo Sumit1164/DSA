cd4b7bbf4442bf6d6629c4a7df0c8b79da986381
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-07 16:14:53 +0530

    Functional Approach 2

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -2,28 +2,34 @@
 using namespace std;
 
 
-void fun3(int n){
-    cout << n << " Days left for birthday\n";
-}
-void fun2(int n)
-{
-    cout << n << " Days left for birthday\n";
-}
-
-void fun1(int n)
-{
-    cout << n << " Days left for birthday\n";
-}
 
 void fun0(int n)
 {
     cout << "happy birthday\n";
 }
+void fun1(int n)
+{
+    cout << n << " Days left for birthday\n";
+    fun0(0);
+}
+void fun2(int n)
+{
+    cout << n << " Days left for birthday\n";
+    fun1(n - 1);
+}
+
+void fun3(int n){
+    cout << n << " Days left for birthday\n";
+    fun2(n - 1);
+}
+
+
 
 
 
 void hBW(int n)
 {
+    // Base Case
     if(n==0)
     {
         cout << "Happy Birthday!"<<endl;
@@ -49,10 +55,13 @@ int main()
 
 
     // Functional Approach
+    // fun3(3);
+    // fun2(2);
+    // fun1(1);
+    // fun0(0);
+
+    // Functional Approach 2
     fun3(3);
-    fun2(2);
-    fun1(1);
-    fun0(0);
 
     // Recursive Approach
     // hBW(n);
