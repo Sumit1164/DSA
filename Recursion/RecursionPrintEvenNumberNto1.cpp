7c2c9de9df70ead81a27e1abb9325a8e4fc46a19
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-08 00:42:30 +0530

    Print Even Number n to 1 using recursive approach

--- Recursion/RecursionPrintEvenNumberNto1.cpp
+++ Recursion/RecursionPrintEvenNumberNto1.cpp
@@ -1,19 +1,40 @@
 #include<iostream>
 using namespace std;
+
+void print(int n)
+{
+    if(n==2)
+    {
+        cout << n << endl;
+        return;
+    }
+    cout << n << endl;
+    print(n - 2);
+}
+
 int main()
 {
     // Print Even Number n to 1 using itrative approach
     int n;
     cout << "Enter the number: ";
     cin >> n;
-    if(n%2==0){
-        for (int i = n; i >=2; i-=2)
-        {
-            cout << i << endl;
-        }
+    // if(n%2==0){
+    //     for (int i = n; i >=2; i-=2)
+    //     {
+    //         cout << i << endl;
+    //     }
         
+    // }
+    // else{
+    //     cout << "Please Enter Even Number!";
+    // }
+
+    // Print Even Number n to 1 using recursive approach
+
+    if(n%2==0){
+        print(n);
     }
     else{
-        cout << "Bakchodi mat kar be laude!";
+        cout << "Please Enter an Even Number!";
     }
 }
