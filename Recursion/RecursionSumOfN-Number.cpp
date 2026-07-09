dadac43c38a5c7667b6e09dba354f26d3dd3f43b
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-09 15:45:11 +0530

    find the sum of n natural numbers using iterative approach

---
+++ Recursion/RecursionSumOfN-Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int sum = 0;
+    
+    // Iterative approach
+    for (int i = 0; i <= n; i++)
+    {
+        sum += i;
+    }
+    cout << sum;
+}
