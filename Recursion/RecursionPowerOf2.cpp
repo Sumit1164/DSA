3dd9bc567672d634d5e4bdc3e0b7483226975a82
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-10 00:58:34 +0530

    find the power of 2^n  using Iterative approach, where n is natural number

--- Recursion/RecursionPowerOf2.cpp
+++ Recursion/RecursionPowerOf2.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
 using namespace std;
 
+int pows(int num, int n)
+{
+    if(n==1){
+        return num;
+    }
+    return num * pows(num, n - 1);
+}
+
 int main()
 {
-    int n = 5;
-    int sum = 2;
+    int powerOf2;
+    cout << "Enter the power of 2:  ";
+    cin >> powerOf2;
 
     // Iterative approach 
-    for (int i = 1; i <=n; i++)
-    {
-        sum +=2* i;
-    }
-    cout << sum;
+    // for (int i = 1; i <=n; i++)
+    // {
+    //     sum +=2* i;
+    // }
+    // cout << sum;
+
+    // Recursive approach
+    cout<< pows(2, powerOf2);
 }
