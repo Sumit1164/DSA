d699e3d966d54bfba0358aa7e67e56d921d506cd
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-10 01:25:37 +0530

    find the sum of square of n natural number using Recursive Approach

--- Recursion/RecursionSumOfSquareOf-N-NaturalNumber.cpp
+++ Recursion/RecursionSumOfSquareOf-N-NaturalNumber.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
+
+int sqrN(int n)
+{
+    if(n==1)
+        return 1;
+    return n * n + sqrN(n - 1);
+}
+
 int main()
 {
     int n = 5;
@@ -11,4 +19,9 @@ int main()
         sum += i*i;
     }
     cout << sum;
+    cout << endl;
+
+
+    // Recursive Approach
+    cout<<sqrN(n);
 }
