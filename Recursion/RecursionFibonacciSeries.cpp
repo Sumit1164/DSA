9d424d194bb20af7b5145fcaf52a9274409351b3
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-10 13:20:56 +0530

    Implement Fibonacci series using recursive approach

--- Recursion/RecursionFibonacciSeries.cpp
+++ Recursion/RecursionFibonacciSeries.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+void fibo(int first, int second, int n)
+{
+    if (n == 1)
+    {
+        return;
+    }
+    int sum = first + second;
+    first = second;
+    second = sum;
+    cout << sum << " ";
+    fibo(first, second, n - 1);
+}
+
+
 int main()
 {
     int first = 0, second = 1, sum = 0;
@@ -8,12 +22,17 @@ int main()
     cout << "Enter the length of the Fibonacci Index: ";
     cin >> n;
 
+    // iteration approach
     cout << first << " " << second << " ";
-    for (int i = 2; i <= n; i++)
-    {
-        sum = first + second;
-        first = second;
-        second = sum;
-        cout << sum << " ";
-    }
+    // for (int i = 2; i <= n; i++)
+    // {
+    //     sum = first + second;
+    //     first = second;
+    //     second = sum;
+    //     cout << sum << " ";
+    // }
+
+
+    // recursive approach
+    fibo(first, second, n);
 }
