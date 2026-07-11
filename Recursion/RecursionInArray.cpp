93f890ffe28116e1900ff67867a1219da0b88063
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-11 21:36:07 +0530

    Print array in reverse order using recursion

--- Recursion/RecursionInArray.cpp
+++ Recursion/RecursionInArray.cpp
@@ -14,18 +14,32 @@ using namespace std;
 
 
 // 2nd way to print array
-void x(int arr[], int index)
+// void x(int arr[], int index)
+// {
+//     cout << arr[index] << " ";
+// }
+
+
+
+
+
+//  Print array in reverse order 
+void print(int arr[], int index, int n)
 {
+    if(index ==n)
+        return;
+
+    print(arr, index + 1, n);
     cout << arr[index] << " ";
 }
 
 int main()
 {
     int arr[] = {3, 4, 5, 6, 7};
-    // print(arr, 0, 5);
+    print(arr, 0, 5);
 
 
     // 2nd way to print array
-    for (int i = 0; i < 5; i++)
-        x(arr, i);
+    // for (int i = 0; i < 5; i++)
+    //     x(arr, i);
 }
