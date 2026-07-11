02ae20af8a74ec3b64f0195af4ef263a6883229b
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-11 23:51:08 +0530

    Using recursive approach to print the sum of element

---
+++ Recursion/RecursionSumOfAllElement1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Using recursive approach to print the sum of element
+
+int print(int arr[], int indx, int n)
+{
+    if(indx == n)
+    return 0;
+
+    return arr[indx]+ print(arr, indx + 1, n);
+}
+
+int main()
+{
+    int arr[] = {3, 4, 5, 8, 2};
+    cout << print(arr, 0, 5);
+}
