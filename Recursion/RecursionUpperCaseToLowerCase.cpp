5d413fc863a08ec741be76b334034f7f0c50a6d4
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-13 00:11:47 +0530

    using recursive approach to convert upper case to lower case string

---
+++ Recursion/RecursionUpperCaseToLowerCase.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void utoL(string &str, int index)
+{
+    if(index==-1)
+        return;
+    str[index] = 'a' + str[index] - 'A';
+    utoL(str, index - 1);
+}
+
+int main()
+{
+    string str = "SUMIT";
+    utoL(str, 4);
+    cout << str;
+}
