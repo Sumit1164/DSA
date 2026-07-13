39ccfba10124c9e24fcf87bd0220e9c4634707b4
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-13 23:48:23 +0530

    using recursive approach to find the element in given in array using linear search recursive

---
+++ Recursion/RecursionLinearSearch.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool linearSearch(int arrr[], int index, int x, int n)
+{
+    if(index ==n)
+        return 0;
+    if(arrr[index] == x)
+        return 1;
+    return linearSearch(arrr, index + 1, x, n);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int find = 5;
+    cout<< linearSearch(arr, 0, find, 7);
+}
