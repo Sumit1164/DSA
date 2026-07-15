8eb604c436dbd778977ba79a98dc1a36db618d2a
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-15 23:59:34 +0530

    Merge Sort

--- Sorting/mergeSORT.cpp
+++ Sorting/mergeSORT.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+void mergeSort(int arr[], int start, int end){
+    // Mid calculate
+
+    // Divede left
+    // Divede right
+}
+
 int main(){
     // Merge Sort
     cout << " Merge Sort ";
