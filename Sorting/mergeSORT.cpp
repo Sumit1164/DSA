e5093a2b23525856c2e776dfc28558f67fa07bfa
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-17 00:53:15 +0530

    Merge Sort complete

--- Sorting/mergeSORT.cpp
+++ Sorting/mergeSORT.cpp
@@ -1,7 +1,55 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+
+void merge (int arr[], int start, int mid, int end)
+{
+    vector<int> arrtemp(end - start + 1);
+
+    int left = start, right = mid + 1, index = 0;
+
+    while (left<=mid && right<=end)
+    {
+        if(arr[left]<=arr[right])
+        {
+            arrtemp[index] = arr[left];
+            index++, left++;
+        }
+        else
+        {
+            arrtemp[index] = arr[right];
+            index++, right++;
+        }
+    }
+    // Left array is not empty yet
+    while(left<=mid)
+    {
+        arrtemp[index] = arr[left];
+        index++, left++;
+    }
+    // right array is not empty yet
+    while (right <= end)
+    {
+        arrtemp[index] = arr[right];
+        index++, right++;
+    }
+
+    // Put these value is array
+    index = 0;
+    while (start<=end)
+    {
+        arr[start] = arrtemp[index];
+        start++, index++;
+    }
+}
+
+
 void mergeSort(int arr[], int start, int end){
+
+    if(start==end)
+        return;
+
     // Mid calculate
     int mid = start + (end - start) / 2;
 
@@ -10,11 +58,21 @@ void mergeSort(int arr[], int start, int end){
 
     // Divede right
     mergeSort(arr, mid + 1, end);
+
+    // merge sort call
+    merge(arr, start, mid, end);
 }
+
 
 int main(){
     // Merge Sort
     cout << " Merge Sort ";
-    int arr[] = {};
-    mergeSort(arr, 0, 7);
+    int arr[] = {6,3,1,2,8,9,10,7,3,12};
+    mergeSort(arr, 0, 9);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
 }
